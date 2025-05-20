92f1ecc6666bbe028856bd332747e2b858198fd8
Sebastian Rafael Belando <belandobaste@gmail.com>
2025-05-20 19:54:52 +0800

    Update

--- Musical-Chord-Application/Program.cs
+++ Musical-Chord-Application/Program.cs
@@ -4,25 +4,23 @@ namespace MusicalChordApplication;
 
 internal class Program
 {
-
+    static string[] menuAction = { "[1]Add Chord",
+                                   "[2]Edit Chord",     
+                                   "[3]Delete Chord", 
+                                   "[4]Search Chord", 
+                                   "[5]Show All Chords",
+                                   "[6]Generate Chord Progression", 
+                                   "[7]Exit" };
     static void Main(string[] args)
     {
         List<string> listChordName = new List<string>();
         List<string> listChordType = new List<string>();
 
-        string[] menuAction = { "[1]Add Chord", "[2]Edit Chord", "[3]Delete Chord", "[4]Search Chord", "[5]Show All Chords", 
-                                "[6]Create Chord Progression", "[7]Exit" };
-
         bool loop = true;
 
         while (loop)
         {
-            Console.WriteLine("-Musical Chord Application-");
-            Console.WriteLine("Actions:");
-            foreach (var menu in menuAction)
-            {
-                Console.WriteLine(menu);
-            }
+            DisplayMenuAction();
 
             Console.Write("Choose Action: ");
             int userAction = Convert.ToInt32(Console.ReadLine());
@@ -47,102 +45,136 @@ internal class Program
                     ListAllChord(listChordName, listChordType);
                     break;
                 case 6:
-                    Console.WriteLine("...\n");
+                    GenerateChordProgression(listChordName, listChordType);
                     break;
                 case 7:
                     loop = false;
                     break;
             }
         }
+    }
 
+    static void DisplayMenuAction()
+    {
+        Console.WriteLine("-Musical Chord Application-");
+        Console.WriteLine("Actions:");
 
-        static void AddChord(List<string> names, List<string> types)
+        foreach (var menu in menuAction)
         {
-            Console.Write("Enter Chord Name: ");
-            string name = Console.ReadLine();
-            Console.Write("Enter Chord Type: ");
-            string type = Console.ReadLine();
-            names.Add(name);
-            types.Add(type);
-            Console.WriteLine("Chord Added Successfully!\n");
+            Console.WriteLine(menu);
+        }
+    }
+
+    static void AddChord(List<string> chordNames, List<string> chordTypes)
+    {
+        Console.Write("Enter Chord Name: ");
+        string name = Console.ReadLine().ToUpper();
+        Console.Write("Enter Chord Type: ");
+        string type = Console.ReadLine().ToUpper();
+        chordNames.Add(name);
+        chordTypes.Add(type);
+        Console.WriteLine("Chord Added Successfully!\n");
+    }
+
+    static void EditChord(List<string> chordNames, List<string> chordTypes)
+    {
+        Console.Write("Enter Chord Name To Edit: ");
+        string oldName = Console.ReadLine().ToUpper();
+
+        int index = chordNames.IndexOf(oldName);
+        if (index != -1)
+        {
+            Console.Write("New Chord Name: ");
+            string newName = Console.ReadLine().ToUpper();
+            Console.Write("New Chord Type: ");
+            string newType = Console.ReadLine().ToUpper();
+            chordNames[index] = newName;
+            chordTypes[index] = newType;
+            Console.WriteLine("Chord Updated Successfully!\n");
+        }
+        else
+        {
+            Console.WriteLine("Chord Not Found!\n");
+        }
+    }
+
+    static void DeleteChord(List<string> chordNames, List<string> chordTypes)
+    {
+        Console.Write("Enter Chord Name To Delete: ");
+        string name = Console.ReadLine().ToUpper();
+
+        int index = chordNames.IndexOf(name);
+        if (index != -1)
+        {
+            chordNames.RemoveAt(index);
+            chordTypes.RemoveAt(index);
+            Console.WriteLine("Chord Deleted Successfully!\n");
+        }
+        else
+        {
+            Console.WriteLine("Chord Not Found!\n");
+        }
+    }
+
+    static void SearchChord(List<string> chordNames, List<string> chordTypes)
+    {
+        Console.Write("Enter Chord Name To Search: ");
+        string name = Console.ReadLine().ToUpper();
+
+        int index = chordNames.IndexOf(name);
+        if (index != -1)
+        {
+            Console.WriteLine($"Chord Found: {chordNames[index]} - {chordTypes[index]}");
+        }
+        else
+        {
+            Console.WriteLine("Chord Not found!\n");
+        }
+    }
+
+    static void ListAllChord(List<string> chordNames, List<string> chordTypes)
+    {
+        if (chordNames.Count == 0)
+        {
+            Console.WriteLine("No Chords Available!\n");
+            return;
         }
 
-
-        static void EditChord(List<string> names, List<string> types)
+        Console.WriteLine("List of All Chords:");
+        for (int i = 0; i < chordNames.Count; i++)
         {
-            Console.Write("Enter Chord Name To Edit: ");
-            string oldName = Console.ReadLine();
-
-            int index = names.IndexOf(oldName);
-            if (index != -1)
-            {
-                Console.Write("New Chord Name: ");
-                string newName = Console.ReadLine();
-                Console.Write("New Chord Type: ");
-                string newType = Console.ReadLine();
-                names[index] = newName;
-                types[index] = newType;
-                Console.WriteLine("Chord Updated Successfully!\n");
-            }
-            else
-            {
-                Console.WriteLine("Chord Not Found!\n");
-            }
+            Console.WriteLine($"{chordNames[i]} ({chordTypes[i]})");
         }
+        Console.WriteLine("");
+    }
 
-
-        static void DeleteChord(List<string> names, List<string> types)
+    static void GenerateChordProgression(List<string> chordNames, List<string> chordTypes)
+    {
+        if (chordNames.Count == 0)
         {
-            Console.Write("Enter Chord Name To Delete: ");
-            string name = Console.ReadLine();
-
-            int index = names.IndexOf(name);
-            if (index != -1)
-            {
-                names.RemoveAt(index);
-                types.RemoveAt(index);
-                Console.WriteLine("Chord Deleted Successfully!\n");
-            }
-            else
-            {
-                Console.WriteLine("Chord Not Found!\n");
-            }
+            Console.WriteLine("No Chords Available!\n");
+            return;
         }
-
-
-        static void SearchChord(List<string> names, List<string> types)
+        else if (chordNames.Count < 4)
         {
-            Console.Write("Enter Chord Name To Search: ");
-            string name = Console.ReadLine();
-
-            int index = names.IndexOf(name);
-            if (index != -1)
-            {
-                Console.WriteLine($"Chord Found: {names[index]} - {types[index]}");
-            }
-            else
-            {
-                Console.WriteLine("Chord Not found!\n");
-            }
+            Console.WriteLine("Not Enough Chords. Enter a Total of Four Chords!\n");
+            return;
         }
-
-
-        static void ListAllChord(List<string> names, List<string> types)
+        else
         {
-            if (names.Count == 0)
-            {
-                Console.WriteLine("No Chords Available!\n");
-                return;
-            }
+            Random randomChords = new Random();
 
-            Console.WriteLine("Chord Progression:");
-            for (int i = 0; i < names.Count; i++)
+            Console.WriteLine("Chord Progression: ");
+
+            for (int i = 0; i < 4; i++)
             {
-                Console.WriteLine($"{names[i]} ({types[i]})");
-                Console.WriteLine("");
+                int randomChordName = randomChords.Next(chordNames.Count);
+                Console.WriteLine("[" + chordNames[randomChordName] + "]");
             }
+            Console.WriteLine("");
         }
 
     }
-
 }
+
+
